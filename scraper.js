f156126984888591e2557b1779a22861e75cf0fc
heatherjang <heather.m.jang@gmail.com>
2015-03-17 23:02:38 +0000

    Moved makeImgObj to separate function

--- scraper.js
+++ scraper.js
@@ -1,42 +1,41 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var url = process.argv[2];
 
-  var request = require('request');
-  var cheerio = require('cheerio');
-  var fs = require('fs');
-  var url = process.argv[2];
-
-
-  function callback(error, response, body){
-    if (error){
-      return;
-    }
-    var $ = cheerio.load(body);
-
-    var absURL, permission, type;
-    var jsonFile = [];
-    
-    $('tr').filter(function(){
-      var json = { absURL: "", permission: "", type: ""};
-      var row = $(this);
-      var relURL = row.find('td').find('a').attr('href');
-      absURL = "http://substack.net" + relURL;
-      permission = row.find('td').find('code').first().text();
-      var fileSplit = relURL.toString().split('.');
-      var len = fileSplit.length;
-      type = fileSplit[len-1];
-
-      json.absURL = absURL;
-      json.permission = permission;
-      json.type = type;
-
-      jsonFile.push(json)
-
-    });
-    fs.writeFile('webscrape.json', JSON.stringify(jsonFile, null, 4), function(err){
-      console.log('Scrape successful! - Check your project directory for the webscrape.json file');
-    });
-
+function scraper(err, response, body){
+  if (err){
+    console.log("Error: ", err)
+    return;
   }
+  var $ = cheerio.load(body);
+  var objArray = [];
+  
+  $('tr').each(function(){
+    var absURL, permission, type;
+    var row = $(this);
 
-  request(url, callback);
+    var relURL = row.find('td').find('a').attr('href');
+    absURL = "http://substack.net" + relURL;
+    permission = row.find('td').find('code').first().text();
+    var fileSplit = relURL.toString().split('.');
+    var len = fileSplit.length;
+    type = fileSplit[len-1];
 
-// }
+    var imgObj = makeImgObj(absURL, permission, type)
+
+    objArray.push(imgObj)
+  });
+
+  // fs.writeFile('webscrape.json', JSON.stringify(objArray, null, 4), function(err){
+  //   console.log('Scrape successful! - Check your project directory for the webscrape.json file');
+  // });
+
+}
+
+function makeImgObj(absURL, permission, type){
+  var imgObj = { absURL: absURL, permission: permission, type: type };
+  return imgObj;
+}
+
+request(url, scraper);
