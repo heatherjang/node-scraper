4dad90d2e01d008c6ead4182ce18f6c323777209
heatherjang <heather.m.jang@gmail.com>
2015-03-17 19:37:24 +0000

    Scraper returns absolute url of images

--- scraper.js
+++ scraper.js
@@ -6,6 +6,14 @@ request(url, callback);
 
 function callback(error, response, body){
     if (!error && response.statusCode == 200) {
-    console.log(body)
+      // console.log(body);
+      $ = cheerio.load(body);
+      var absURL, permission, type;
+      var json = { absURL: "", permission: "", type: ""};
+      $('td').filter(function(){
+        var image = $(this);
+        url = "http://substack.net" + image.find('a').attr('href');
+        console.log(url);
+      })
   }
 }
