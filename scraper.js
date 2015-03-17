2af9f023745caecf3920fbce5c5fb179475aa1e7
heatherjang <heather.m.jang@gmail.com>
2015-03-17 22:32:38 +0000

    Webscraper results write to json file

--- scraper.js
+++ scraper.js
@@ -1,19 +1,42 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var url = process.argv[2];
 
-request(url, callback);
+  var request = require('request');
+  var cheerio = require('cheerio');
+  var fs = require('fs');
+  var url = process.argv[2];
 
-function callback(error, response, body){
-    if (!error && response.statusCode == 200) {
-      // console.log(body);
-      $ = cheerio.load(body);
-      var absURL, permission, type;
+
+  function callback(error, response, body){
+    if (error){
+      return;
+    }
+    var $ = cheerio.load(body);
+
+    var absURL, permission, type;
+    var jsonFile = [];
+    
+    $('tr').filter(function(){
       var json = { absURL: "", permission: "", type: ""};
-      $('td').filter(function(){
-        var image = $(this);
-        url = "http://substack.net" + image.find('a').attr('href');
-        console.log(url);
-      })
+      var row = $(this);
+      var relURL = row.find('td').find('a').attr('href');
+      absURL = "http://substack.net" + relURL;
+      permission = row.find('td').find('code').first().text();
+      var fileSplit = relURL.toString().split('.');
+      var len = fileSplit.length;
+      type = fileSplit[len-1];
+
+      json.absURL = absURL;
+      json.permission = permission;
+      json.type = type;
+
+      jsonFile.push(json)
+
+    });
+    fs.writeFile('webscrape.json', JSON.stringify(jsonFile, null, 4), function(err){
+      console.log('Scrape successful! - Check your project directory for the webscrape.json file');
+    });
+
   }
-}
+
+  request(url, callback);
+
+// }
